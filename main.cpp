fe852a0c735a8b4163e617dc383390b2b9ff9d2f
Milan Panta <milan.panta@icloud.com>
2024-09-01 19:56:03 -0400

    remove cell type, just store heights as integers

--- main.cpp
+++ main.cpp
@@ -11,42 +11,31 @@ const int CELL_WIDTH = 25;
 const int MIN_CELL_HEIGHT = 50;
 const int TOTAL_CELLS = SCREEN_WIDTH / CELL_WIDTH;
 
-class Cell {
-public:
-  int height;
-  int x;
-  int y;
-
-  void Draw() { DrawRectangle(x, y, CELL_WIDTH, height, WHITE); }
-};
-
-Cell generateCell(int i) {
-  Cell r;
-  r.height = rand() % (SCREEN_HEIGHT - MIN_CELL_HEIGHT - PADDING * 2) +
-             MIN_CELL_HEIGHT;
-  r.x = CELL_WIDTH * i + PADDING;
-  r.y = SCREEN_HEIGHT - r.height - PADDING;
-  return r;
+int generateCell() {
+  int height = rand() % (SCREEN_HEIGHT - MIN_CELL_HEIGHT - PADDING * 2) +
+               MIN_CELL_HEIGHT;
+  return height;
 }
 
-vector<Cell> cells(TOTAL_CELLS);
+vector<int> cells(TOTAL_CELLS);
 
-void swapCells(Cell &a, Cell &b) {
-  Cell tmp = a;
-  a.height = b.height;
-  a.x = b.x;
-  a.y = b.y;
-  b.height = tmp.height;
-  b.x = tmp.x;
-  b.y = tmp.y;
+void DrawCells(int a, int b) {
+  // draw cells with a, b being most recent swaps
+  BeginDrawing();
+  ClearBackground(BLACK);
+  for (int i = 0; i < TOTAL_CELLS; i++) {
+    DrawRectangle(PADDING + i * CELL_WIDTH, SCREEN_HEIGHT - cells[i] - PADDING,
+                  CELL_WIDTH, cells[i], i == a || i == b ? RED : WHITE);
+  }
+  EndDrawing();
 }
 
 void bubbleSort() {
   for (int i = 0; i < TOTAL_CELLS; i++) {
     for (int j = 0; j < TOTAL_CELLS - i - 1; j++) {
-      if (cells[j].height > cells[j + 1].height) {
-        swapCells(cells[i], cells[i + 1]);
-        swap(cells[i], cells[i + 1]);
+      if (cells[j] > cells[j + 1]) {
+        DrawCells(j, j + 1);
+        swap(cells[j], cells[j + 1]);
       }
     }
   }
@@ -54,20 +43,14 @@ void bubbleSort() {
 
 int main() {
   for (int i = 0; i < TOTAL_CELLS; i++) {
-    cells[i] = generateCell(i);
+    cells[i] = generateCell();
   }
   cout << "Commence Sorting" << endl;
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sorting Visualizer");
   SetTargetFPS(60);
 
   while (WindowShouldClose() == false) {
-    BeginDrawing();
-
-    ClearBackground(BLACK);
-    for (int i = 0; i < TOTAL_CELLS; i++) {
-      cells[i].Draw();
-    }
-    EndDrawing();
+    bubbleSort();
   }
 
   CloseWindow();
